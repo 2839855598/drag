4d4869afad709518668a79235301a6175fe1e00a
2839855598 <2839855598@github.com>
2018-02-27 22:01:34 +0800

    change succeed

--- drag.js
+++ drag.js
@@ -3,8 +3,10 @@ function drag(id)
 	var oBox=document.getElementById(id);
 	var disX=0;
 	var disY=0;
-	document.onmousemove=function()
+	document.onmousemove=function(ev)
 	{
-
+		var ev=ev||window.event;
+        disX=ev.clientX-oBox.offsetLeft;
+        disY=ev.clientY-oBox.offsetTop;
 	}
 }
