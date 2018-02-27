7bb983a60883c19a6b0c430d4e4c1a5f81518e10
2839855598 <2839855598@github.com>
2018-02-27 21:24:41 +0800

    new commit

--- drag.js
+++ drag.js
@@ -1,4 +1,10 @@
-function drag()
+function drag(id)
 {
-	
+	var oBox=document.getElementById(id);
+	oBox.onmousedown=function(ev)
+	{
+		var ev=ev||window.event;
+		var disX=ev.clientX-oBox.offsetLeft;
+		var disY=ev.clientY-oBox.offsetTop;
+	}
 }
